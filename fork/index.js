0d86c729e55123ab42c2059a2c82ca624bb70eb4
nancheng <1443837536@qq.com>
2023-03-18 03:50:03 +0800

    update.

---
+++ fork/index.js
@@ -0,0 +1,11 @@
+const child_process = require("child_process");
+const path = require("path");
+
+const fork = (fpath, args, opts) => {
+  return child_process.fork(fpath, args, {
+    execPath: "D:/poj/node16170bit32/node.exe",
+    ...opts,
+  });
+};
+
+module.exports = fork
---
+++ rsqy/index.js
@@ -0,0 +1,2 @@
+const { Screenshot } = require("./rs-qy.win32-ia32-msvc.node");
+module.exports = Screenshot
